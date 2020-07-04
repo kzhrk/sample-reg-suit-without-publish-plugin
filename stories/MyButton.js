5c7a97076dab2b194a59c314dc1dd2e3ca2443a3
Kobayashi Kazuhiro <info@kzhrk.com>
2020-07-05 02:21:30 +0900

    Work/test actions (#2)
    * Add workflow
    * Remove reg-suit

--- stories/MyButton.js
+++ stories/MyButton.js
@@ -8,8 +8,8 @@ export default {
         borderRadius: 3,
         backgroundColor: '#FFFFFF',
         cursor: 'pointer',
-        fontSize: 15,
-        padding: '3px 10px',
+        fontSize: 22,
+        padding: '30px 33px',
         margin: 10,
       },
     };
