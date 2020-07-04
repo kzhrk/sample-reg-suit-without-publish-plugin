fe4a3b5068349eb96135be82c1b059beb31f3f5e
kzhrk <info@kzhrk.com>
2020-07-05 00:58:51 +0900

    update button

--- stories/MyButton.js
+++ stories/MyButton.js
@@ -8,8 +8,8 @@ export default {
         borderRadius: 3,
         backgroundColor: '#FFFFFF',
         cursor: 'pointer',
-        fontSize: 15,
-        padding: '3px 30px',
+        fontSize: 20,
+        padding: '3px 50px',
         margin: 10,
       },
     };
